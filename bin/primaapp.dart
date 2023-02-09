095dd46e1f44347ed06a99a6e793a0118612a02a
GianITS <gianluca.mattiello.studio@fitstic-edu.com>
2023-02-09 18:46:01 +0100

    implementata chiamata http

--- bin/primaapp.dart
+++ bin/primaapp.dart
@@ -1,15 +1,21 @@
-import 'package:primaapp/primaapp.dart' as primaapp;
+import 'dart:convert';
+
+// import 'package:primaapp/primaapp.dart' as primaapp;
 import 'package:http/http.dart' as http;
 import 'dart:io';
 
-void main(List<String> arguments) {
-  // print('Quanti numeri vuoi stampare?');
-  // int n = int.parse(stdin.readLineSync()!);
-  // List<int> miaSerie = primaapp.Fibonacci(calc: n).serieFibonacci();
-  // for (var i = 0; i < miaSerie.length; i++) {
-  //   stdout.write(miaSerie[i]);
-  //   if (i != miaSerie.length - 1) {
-  //     stdout.write(' - ');
-  //   }
-  // }
+Future main(List<String> arguments) async {
+  getQuote();
+}
+
+void getQuote() async {
+  final url = Uri.parse('https://the-one-api.dev/v2/quote');
+  final res = await http.get(url, headers: {
+    HttpHeaders.authorizationHeader: 'Bearer iUip64JAjBbsn1FB1akD',
+  });
+
+  final data = jsonDecode(res.body) as Map<String, dynamic>;
+
+  print(data['docs'][23]['dialog']);
+  //Gollum' Gollum' Gollum', and we wept precious. We wept to be so alone.
 }
