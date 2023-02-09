238e7dde263bd1e4070079329819d8bf359b2764
GianITS <gianluca.mattiello.studio@fitstic-edu.com>
2023-02-09 18:21:39 +0100

    import http

--- bin/primaapp.dart
+++ bin/primaapp.dart
@@ -1,14 +1,15 @@
 import 'package:primaapp/primaapp.dart' as primaapp;
+import 'package:http/http.dart' as http;
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Quanti numeri vuoi stampare?');
-  int n = int.parse(stdin.readLineSync()!);
-  List<int> miaSerie = primaapp.Fibonacci(calc: n).serieFibonacci();
-  for (var i = 0; i < miaSerie.length; i++) {
-    stdout.write(miaSerie[i]);
-    if (i != miaSerie.length - 1) {
-      stdout.write(' - ');
-    }
-  }
+  // print('Quanti numeri vuoi stampare?');
+  // int n = int.parse(stdin.readLineSync()!);
+  // List<int> miaSerie = primaapp.Fibonacci(calc: n).serieFibonacci();
+  // for (var i = 0; i < miaSerie.length; i++) {
+  //   stdout.write(miaSerie[i]);
+  //   if (i != miaSerie.length - 1) {
+  //     stdout.write(' - ');
+  //   }
+  // }
 }
